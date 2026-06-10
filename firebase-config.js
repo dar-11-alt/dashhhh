2c5a71979758b743281ac8839de064f04f1d038f
dar-11-alt <luceneciodariel5@gmail.com>
2026-06-10 09:37:44 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCHuiriR43mVWwOb7u92TdmbCoQU-weLYQ",
   authDomain: "dashboard-6d4fc.firebaseapp.com",
+  databaseURL: "https://dashboard-6d4fc-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "dashboard-6d4fc",
   storageBucket: "dashboard-6d4fc.firebasestorage.app",
   messagingSenderId: "415408574366",
-  appId: "1:415408574366:web:c6b1304b555d5c386e508f",
-  measurementId: "G-5TV82C41KZ"
+  appId: "1:415408574366:web:c6b1304b555d5c386e508f"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getDatabase(app);
